40f4fc1f5a5fbb29bb999ef2b730f28d9c7e3762
Night Chough <chicken.snuggles@gmail.com>
2021-07-17 13:37:11 -0500

    added typeDefs

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,58 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Sale {
+        _id: ID
+        location: String
+        startDate: String
+        endDate: String
+        startTime: String
+        endTime: String
+        description: String
+        image: String
+    }
+    
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        sales: [Sale]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query{
+        me: User
+        sales: [Sale]
+        sale(_id: ID!): Sale 
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!)
+        addSale(
+            location: String!,
+            startDate: String!,
+            endDate: String!,
+            startTime: String!,
+            endTime: String!,
+            description: String!,
+            image: String,
+        ): Sale
+        updateSale(
+            _id: ID!,
+            location: String,
+            startDate: String,
+            endDate: String,
+            startTime: String,
+            endTime: String,
+            description: String,
+            image: String,
+            ): Sale
+        removeSale(_id: ID!): Sale
+    }
+`
