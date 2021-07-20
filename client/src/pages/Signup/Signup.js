f8fc236b2a31715eac046b8920ce2a0e694d65c0
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-19 20:18:21 -0500

    sign up skeleton

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -3,10 +3,9 @@ import Header from "../../components/Header/Header";
 import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import { ADD_USER } from '../../utils/mutations';
 
 
 const Signup = () => {
@@ -40,6 +39,31 @@ const handleFormSubmit = async (event) => {
     <div className="signup">
       <Header />
       <Nav />
+
+      <form onSubmit={handleFormSubmit}>
+        <div className="">
+          <label htmlFor="firstName">First Name:</label>
+          <input placeholder="First" name="firstName" type="firstName" id="firstName" onChange={handleChange}/>
+        </div>
+
+        <div className="">
+          <label htmlFor="lastName">Last Name:</label>
+          <input  placeholder="Last" name="lastName" type="lastName" id="lastName" onChange={handleChange}/>
+        </div>
+
+        <div className="">
+          <label htmlFor="email">Email:</label>
+          <input placeholder="youremail@test.com" name="email" type="email" id="email" onChange={handleChange} />
+        </div>
+
+        <div className="">
+          <label htmlFor="pwd">Password:</label>
+          <input placeholder="******" name="password" type="password" id="pwd" onChange={handleChange}/>
+        </div>
+        <div className="">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
       <Footer />
     </div>
   );
