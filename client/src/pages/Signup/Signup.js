20a0b9225c9bd8b1042915ba683b6b4aea3c0ad3
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-19 19:57:06 -0500

    signup logic

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -2,8 +2,40 @@ import "./Signup.css";
 import Header from "../../components/Header/Header";
 import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+
 
 const Signup = () => {
+const [formState, setFormState] = useState({ email: '', password: '' });
+const [addUser] = useMutation(ADD_USER);
+
+const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+
   return (
     <div className="signup">
       <Header />
