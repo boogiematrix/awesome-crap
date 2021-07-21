f5b6c3e07d4a7d91b4472f648bac9afde291f23b
Marina <mgdelneri@gmail.com>
2021-07-21 18:37:43 -0500

    Added conditionality in Nav component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,25 +1,32 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
 
 const Nav = () => {
   return (
     <nav className="navbar">
-      {/* TODO: Add conditionals for login and logout */}
-      <Link className="loginLink link" to="login">
-        Log In
-      </Link>
-      <Link className="signupLink link" to="signup">
-        Sign Up
-      </Link>
-      <Link className="postLink link" to="userpost">
-        Post 
-      </Link>
       <Link className="homeLink link" to="/">
         Home
       </Link>
-      <Link className="logoutLink link" to="/">
-        Log Out
-      </Link>
+      {Auth.loggedIn() ? (
+        <>
+          <Link className="postLink link" to="userpost">
+            Post
+          </Link>
+          <Link onClick={Auth.logout} className="logoutLink link" to="/">
+            Log Out
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link className="loginLink link" to="login">
+            Log In
+          </Link>
+          <Link className="signupLink link" to="signup">
+            Sign Up
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
