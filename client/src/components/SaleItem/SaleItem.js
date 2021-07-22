d5e5ce9acd92596493a3c638da8a5426bb635b44
Night Chough <chicken.snuggles@gmail.com>
2021-07-21 20:10:20 -0500

    added data rendering to saleItem

--- client/src/components/SaleItem/SaleItem.js
+++ client/src/components/SaleItem/SaleItem.js
@@ -1,14 +1,38 @@
+import React from 'react'
+import { TOGGLE_INTERESTED_IN } from '../../utils/actions';
+import { useDispatch, useSelector } from 'react-redux';
 import "./SaleItem.css";
 
-const SaleItem = () => {
+const SaleItem = (props) => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state)
+  
+  const {
+    _id,
+    location,
+    startDate,
+    endDate,
+    description,
+    image
+  } = props
+
+  const { savedSale } = state;
+
+  const isInteresting = () => {
+    dispatch({
+      type: TOGGLE_INTERESTED_IN,
+      saleID: _id
+    })
+  }
+
   return (
     <section className="saleItem">
       <div className="saleItemBox">
-        <p>Where?</p>
-        <p>What days?</p>
-        <p>What time?</p>
-        <p>Description</p>
-        <button>Toggle Button</button>
+        <p>{location}</p>
+        <p>{startDate}</p>
+        <p>{endDate}</p>
+        <p>{description}</p>
+        <button onClick={isInteresting}>Toggle Button</button>
       </div>
     </section>
   );
