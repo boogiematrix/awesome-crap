cb1f61c13daac3c04480595c88174a5001263b8a
Marina <mgdelneri@gmail.com>
2021-07-26 19:42:48 -0500

    Fixed image bug

--- client/src/components/SaleItem/SaleItem.js
+++ client/src/components/SaleItem/SaleItem.js
@@ -1,22 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import { useMutation, useQuery } from '@apollo/client';
-import Auth  from '../../utils/auth'
-import { TOGGLE_INTERESTED_IN } from '../../utils/actions';
-import { GET_ME } from '../../utils/queries';
-import { SAVE_SALE, UNSAVE_SALE } from '../../utils/mutations';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import Auth from "../../utils/auth";
+import { TOGGLE_INTERESTED_IN } from "../../utils/actions";
+import { GET_ME } from "../../utils/queries";
+import { SAVE_SALE, UNSAVE_SALE } from "../../utils/mutations";
+import { useDispatch, useSelector } from "react-redux";
 import "./SaleItem.css";
 
-
 const SaleItem = (props) => {
-let userData = {}
+  let userData = {};
 
   const dispatch = useDispatch();
-  const state = useSelector((state) => state)
-  const [saveSale, { error }] = useMutation(SAVE_SALE)
-  const [unsaveSale] = useMutation(UNSAVE_SALE)
-  const { loading, data } = useQuery(GET_ME)
-  
+  const state = useSelector((state) => state);
+  const [saveSale, { error }] = useMutation(SAVE_SALE);
+  const [unsaveSale] = useMutation(UNSAVE_SALE);
+  const { loading, data } = useQuery(GET_ME);
+
   const {
     _id,
     location,
@@ -24,25 +23,27 @@ let userData = {}
     endDate,
     description,
     image,
-    mySavedSales
-  } = props
+    mySavedSales,
+  } = props;
+
+  console.log(image);
 
   const { savedSales } = state;
-  
-  let isInterested = savedSales.includes(_id) || mySavedSales.includes(_id)
+
+  let isInterested = savedSales.includes(_id) || mySavedSales.includes(_id);
 
   const imInterested = async () => {
     dispatch({
       type: TOGGLE_INTERESTED_IN,
       isInterested: isInterested,
-      saleID: _id
-    })
+      saleID: _id,
+    });
     if (isInterested) {
       await unsaveSale({
         variables: {
-          _id: _id
-        }
-      })
+          _id: _id,
+        },
+      });
     } else {
       try {
         await saveSale({
@@ -52,31 +53,48 @@ let userData = {}
             startDate: startDate,
             endDate: endDate,
             description: description,
-            image: image
-          }
-        })
+            image: image,
+          },
+        });
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     }
-  }
-  
-    return (
-      <section className="saleItem">
-      <div className="saleItemBox">
+  };
 
+  return (
+    <section className="saleItem">
+      <div className="saleItemBox">
         <p>{location}</p>
         <p>{startDate}</p>
         <p>{endDate}</p>
         <p>{description}</p>
-        
-          {Auth.loggedIn() ? (isInterested ? (<button className="saleItemBtn" onClick={imInterested}>I'm Aware of This Crap</button>)
-            : <button className="saleItemBtn" onClick={imInterested}>I Want This Crap!</button>
-          ) : (<p></p>)}
+        {image ? (
+          <img
+            src={image}
+            alt=""
+            style={{ maxWidth: "300px", maxHeight: "300px" }}
+          />
+        ) : (
+          <p></p>
+        )}
+
+        {Auth.loggedIn() ? (
+          isInterested ? (
+            <button className="saleItemBtn" onClick={imInterested}>
+              I'm Aware of This Crap
+            </button>
+          ) : (
+            <button className="saleItemBtn" onClick={imInterested}>
+              I Want This Crap!
+            </button>
+          )
+        ) : (
+          <p></p>
+        )}
       </div>
     </section>
   );
-
 };
 
 export default SaleItem;
